a8937b4573bd7bd13418896ed83f6a38b887d8a5
meylisday <victory.sett@gmail.com>
2020-06-19 18:14:27 +0300

    Updated logic for categories

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,7 @@
     <b-sidebar id="sidebar-1" title="Filter" shadow>
       <div class="px-3 py-2">
         <b-form-input v-model="search" placeholder="Enter title"></b-form-input>
-        <Dropdown :label="'Suburb'" :options="suburbs" @on-select="handleselect"></Dropdown>
-        <Dropdown :label="'Stage'" :options="stages" @on-select="handleselect"></Dropdown>
-        <Dropdown :label="'Category'" :options="categories" @on-select="handleselect"></Dropdown>
-        <Dropdown :label="'SubCategory'" :options="subcategories" @on-select="handleselect"></Dropdown>
-        <Dropdown :label="'Status'" :options="statuses" @on-select="handleselect"></Dropdown>
-        <Dropdown :label="'Council'" :options="councils" @on-select="handleselect"></Dropdown>
+        <Dropdown v-for="filter in Object.keys(availableFilters)" :label="filter" :key="filter" :options="availableFilters[filter]" @on-select="handleselect">{{ filter}}</Dropdown>
       </div>
     </b-sidebar>
 
@@ -30,97 +25,33 @@ export default {
   data() {
     return {
       geoDataFilters: {},
-      suburbs: [
-        "SYDNEY",
-        "LISMORE",
-        "ASHFIELD",
-        "PYRMONT",
-        "SYDNEY SOUTH",
-        "DARLING HARBOUR",
-        "CIRCULAR QUAY",
-        "HAYMARKET",
-        "PARRAMATTA",
-        "NORTH SYDNEY",
-        "WINDSOR",
-        "MOONEE BEACH",
-        "CONCORD REPATRIATION HOSPITAL"
-      ],
-      stages: [
-        "DA Approved",
-        "DA Pending",
-        "Construction",
-        "DA Refused",
-        "Other"
-      ],
-      categories: [
-        "HOSPITALITY",
-        "COMMERCIAL PREMISES",
-        "RETAIL",
-        "SOCIAL",
-        "PUBLIC BUILDINGS & FACILITIES",
-        "RESIDENTIAL",
-        "SPORTS",
-        "EDUCATION",
-        "CIVIL ENGINEERING",
-        "ENTERTAINMENT"
-      ],
-      subcategories: [
-        "RESTAURANTS, CANTEENS",
-        "CLUBS (LICENSED), LICENSED PREMISES, CASINOS, TAVERNS",
-        "COMMERCIAL COMPLEXES - SHOPS, OFFICES",
-        "LANDSCAPING, FENCING, ENTRY STATEMENTS, MONUMENTS, FOUNTAINS",
-        "AMENITIES, DRESSING ROOMS",
-        "SHOPS, SHOPPING CENTRES & ARCADES, SUPERMARKETS",
-        "HOTELS, SERVICED APARTMENTS",
-        "OFFICES",
-        "CAR PARKS (MULTI-STOREY)",
-        "FIRE STATIONS, SES",
-        "TAKE-AWAY OUTLETS",
-        "LIBRARY, ARCHIVES, GALLERIES, MUSEUMS, INTERPRETIVE CENTRES, TOURIST INFO",
-        "SWIMMING BATHS & ASSOC. FACILITIES, SAUNAS, SPAS, SOLARIA",
-        "INDOOR SPORTS & LEISURE CENTRES, BOWLING ALLEYS, SQUASH COURTS, GYMNASIUMS",
-        "RELIGIOUS BUILDINGS, CIVIL CHAPELS",
-        "TERTIARY - COLLEGES, INSTITUTES, UNIVERSITIES",
-        "SCHOOLS - PRIMARY, SECONDARY (UP TO YR 12)",
-        "KINDERGARTENS, DAY NURSERIES, CHILD CARE CENTRES",
-        "TUNNELS, SUBWAYS",
-        "LAW COURTS",
-        "CONFERENCE & RECEPTION CENTRES & FACILITIES",
-        "FED, STATE, LOCAL GOV'T OFFICES, EMBASSIES, JOB CENTRES,CIVIC CENTRES, TOWNHALLS",
-        "UNITS, APARTMENTS, FLATS, TOWNHOUSES, VILLAS",
-        "BANKS",
-        "SURGERIES, MEDICAL CENTRES",
-        "BACKPACKER ACCOMMODATION",
-        "THEATRES, CINEMAS",
-        "SPORTS GROUND HARD SURFACE (TENNIS COURTS, BASKETBALL & SKATEPARKS)",
-        "RESIDENTIAL SUBDIVISION",
-        "POWER STATIONS, SUBSTATIONS, WINDFARMS, PHONE EXCHANGES"
-      ],
-      statuses: [
-        "Possible",
-        "Firm",
-        "Commenced",
-        "Deferred",
-        "No further info available",
-        "Abandoned",
-        "Registrations"
-      ],
-      councils: [
-        "SYDNEY",
-        "LISMORE",
-        "ASHFIELD",
-        "PARRAMATTA",
-        "NORTH SYDNEY",
-        "HAWKESBURY",
-        "COFFS HARBOUR",
-        "CANADA BAY"
-      ],
-      search: ""
+      features: [],
+      availableFilters: {},
+      search: "",
+      filterBy: ["Suburb", "Stage", "Category", "SubCategory", "Status", "Council"]
     };
   },
   components: {
     Dropdown,
     Map
+  },
+  mounted() {
+    fetch(
+      "https://bitbucket.org/idda/coding-challenges/raw/88be221f75a1b108c9e5f7222906b2735c147ac8/testBlob.json"
+    )
+      .then(response => response.json())
+      .then(data => {
+        for (let filter of this.filterBy) {
+          let unique = [
+            ...new Set(
+              data.features.map(item => item.properties.project[filter])
+            )
+          ];
+          this.availableFilters = Object.assign({}, this.availableFilters,{
+            [filter]: unique
+          });
+        }
+      });
   },
   methods: {
     handleselect(selected) {
